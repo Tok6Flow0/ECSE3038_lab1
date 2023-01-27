f395b910428622aaf7c96c4501fa3d2fe94ee651
Tok6Flow0 <aaronmopsamuel@gmail.com>
2023-01-27 14:39:44 -0500

    trying to display voltage symbol beside values

--- app2.py
+++ app2.py
@@ -5,5 +5,17 @@ def potential_divider(voltage, resistor_values):
         total_resistance = 0
         for i in resistor_values:
             total_resistance += i
+
         #creating an empty array
+        divided_voltage = []
+        for j in resistor_values:
+            #assigning values to empty array using append operation in python
+            divided_voltage.append(j * voltage / total_resistance)
+            divided_voltage.append("V")
+        return divided_voltage
+
+result = potential_divider(5, [100,330])
+
+print(result)
+
         
