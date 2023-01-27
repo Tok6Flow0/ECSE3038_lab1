f77a825c280a84db7487d7488f30991812c67d73
Tok6Flow0 <aaronmopsamuel@gmail.com>
2023-01-27 15:16:04 -0500

    added final statements

--- app3.py
+++ app3.py
@@ -1,8 +1,23 @@
 def temperature_check(temperature, unit):
     if unit == 'C':
         if temperature < 35:
-            print("3")
+            print("the patient is hypothermic")
         elif temperature > 38:
-            print("4")
+            print("the patient is hyperthermic")
+        elif temperature == 0:
+            print("You are not measuring a patient, but in fact an ice block. You are measuring an ice block")
         else:
-            print("5")
+            print("The patient's temperature is normal")
+    elif unit == 'F':
+        if temperature < 95:
+            print("the patient is hypothermic")
+        elif temperature > 104:
+            print("the patient is hyperthermic")
+        elif temperature == 32:
+            print("changing the temperature to Fahrenheit does not change the fact that you are measuring an ice block")
+        else:
+            print("the patient's temperature is normal")
+    else:
+        print("please enter 'C' for Celcius OR 'F' for Fahrenheit")
+
+temperature_check(0, 'C')
